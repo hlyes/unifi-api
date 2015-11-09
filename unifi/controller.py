b6b0cfbcce51acbb3dac99f0d8b6c27fab6e8d2f
HAMIDOUCHE Lyes <hlyes@users.noreply.github.com>
2015-11-09 18:42:40 +0100

    Disable SSL verification

--- unifi/controller.py
+++ unifi/controller.py
@@ -1,6 +1,9 @@
 
 import ssl
 from functools import wraps
+
+
+
 def sslwrap(func):
     @wraps(func)
     def bar(*args, **kw):
@@ -11,6 +14,7 @@ def sslwrap(func):
     return bar
 
 ssl.wrap_socket = sslwrap(ssl.wrap_socket)
+ssl._create_default_https_context = ssl._create_unverified_context
 
 import cookielib
 import json
